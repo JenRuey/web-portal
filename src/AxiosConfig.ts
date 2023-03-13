c903ca0a3a2d4c9a49749eb36e0bbc6795e9a521
Jen <jenrueyng@gmail.com>
2023-03-13 09:45:24 +0800

    fix access token refresh issue

--- src/AxiosConfig.ts
+++ src/AxiosConfig.ts
@@ -5,13 +5,12 @@ import buildpath from "./constants/route-path";
 import { logout } from "./functions/misc";
 
 const axiosInstance = axios.create();
-const axiosFresh = axios.create();
+const axiosFresh2 = axios.create();
 
 // Add a request interceptor
 axiosInstance.interceptors.request.use(
   function (config) {
-    const token =
-      "Bearer " + localStorage.getItem(localstoragetext.accesstoken);
+    const token = "Bearer " + localStorage.getItem(localstoragetext.accesstoken);
     config.headers.Authorization = token;
     // Do something before request is sent
     console.debug("Request:" + config.url);
@@ -20,12 +19,7 @@ axiosInstance.interceptors.request.use(
   function (error) {
     console.debug(error);
 
-    let errormesssage =
-      (error.data ? error.data.message : null) ||
-      (error.response && error.response.data
-        ? error.response.data.message || error.response.data.error
-        : null) ||
-      error.message;
+    let errormesssage = (error.data ? error.data.message : null) || (error.response && error.response.data ? error.response.data.message || error.response.data.error : null) || error.message;
     console.debug(errormesssage);
     // Do something with request error
     //return Promise.reject(error);
@@ -47,21 +41,16 @@ axiosInstance.interceptors.response.use(
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     //return Promise.reject(error);x
-    let errormesssage =
-      (error.data ? error.data.message : null) ||
-      (error.response && error.response.data
-        ? error.response.data.message || error.response.data.error
-        : null) ||
-      error.message;
+    let errormesssage = (error.data ? error.data.message : null) || (error.response && error.response.data ? error.response.data.message || error.response.data.error : null) || error.message;
     console.debug(errormesssage);
-    if (errormesssage.includes("Invalid JWT Token")) {
+    if (errormesssage.includes("Invalid JWT Token") || errormesssage.includes("Unable to read JSON value")) {
       const originalRequest = error.config;
       originalRequest._retry = true;
-      const access_token = await refreshAccessToken();
+      let access_token = await refreshAccessToken();
       if (access_token) {
-        axiosFresh.defaults.headers.common["Authorization"] =
-          "Bearer " + access_token;
-        return axiosFresh(originalRequest);
+        if (access_token === "skip this") access_token = localStorage.getItem(localstoragetext.accesstoken);
+        axiosInstance.defaults.headers.common["Authorization"] = "Bearer " + access_token;
+        return axiosInstance(originalRequest);
       } else {
         logout();
         window.location.href = buildpath.login;
@@ -76,23 +65,19 @@ axiosInstance.interceptors.response.use(
 async function refreshAccessToken(): Promise<string | null> {
   let refreshtoken = localStorage.getItem(localstoragetext.refreshtoken);
   if (refreshtoken) {
+    let usedToken = refreshtoken;
     try {
-      let resp = await axiosInstance.get(
-        process.env.REACT_APP_API_URL +
-          "/token/renew?token=" +
-          encodeURIComponent(refreshtoken)
-      );
-      localStorage.setItem(
-        localstoragetext.accesstoken,
-        resp.data.access_token
-      );
-      localStorage.setItem(
-        localstoragetext.refreshtoken,
-        resp.data.refresh_token
-      );
+      let usedToken = refreshtoken;
+      let resp = await axiosFresh2.get(process.env.REACT_APP_API_URL + "/token/renew?token=" + encodeURIComponent(usedToken));
+      localStorage.setItem(localstoragetext.accesstoken, resp.data.access_token);
+      localStorage.setItem(localstoragetext.refreshtoken, resp.data.refresh_token);
       return resp.data.access_token;
     } catch (err) {
-      return null;
+      if (usedToken === localStorage.getItem(localstoragetext.refreshtoken)) {
+        return null;
+      } else {
+        return "skip this";
+      }
     }
   } else {
     return null;
